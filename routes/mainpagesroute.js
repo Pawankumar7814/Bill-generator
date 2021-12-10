217307cde5d1ab1328fb4decd9d95fec1b7fcf69
Pawankumar7814 <pawank4285@gmail.com>
2021-12-10 21:12:18 +0530

    add column

--- routes/mainpagesroute.js
+++ routes/mainpagesroute.js
@@ -3,11 +3,11 @@ let router = express.Router();
 var templateToPdf = require('html-template-pdf');
 //Route 1
 router.get("/", (req, res) => {
-    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "" });
+    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "", v8: "" });
 })
 
 router.get("/index", (req, res) => {
-    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "" });
+    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "", v8: "" });
 })
 
 router.post("/print", (req, res) => {
@@ -18,7 +18,8 @@ router.post("/print", (req, res) => {
     let state = req.body.state;
     let post = req.body.postalcode;
     let amount = req.body.amt;
-    res.status(200).render("../views/mainpages/index.ejs", { v1: F_name, v2: l_name, v3: add, v4: City, v5: state, v6: post, v7: amount });
+    let qty = req.body.qty;
+    res.status(200).render("../views/mainpages/index.ejs", { v1: F_name, v2: l_name, v3: add, v4: City, v5: state, v6: post, v7: amount, v8: qty });
 })
 
 module.exports = router;
