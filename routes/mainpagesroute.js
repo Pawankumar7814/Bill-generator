a552dff4fd3a705f69352dbc6951688e6b537113
Pawankumar7814 <pawank4285@gmail.com>
2021-12-17 15:49:39 +0530

    update

--- routes/mainpagesroute.js
+++ routes/mainpagesroute.js
@@ -3,27 +3,32 @@ let router = express.Router();
 
 //Route 1
 router.get("/", (req, res) => {
-    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "", v8: "" });
+    res.status(200).render("../views/mainpages/index.ejs", { data: "" });
 })
 
 router.get("/index", (req, res) => {
-    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "", v8: "" });
+    res.status(200).render("../views/mainpages/index.ejs", { data: "" });
 });
 
 router.get("/*", (req, res) => {
-    res.status(200).render("../views/mainpages/index.ejs", { v1: "", v2: "", v3: "", v4: "", v5: "", v6: "", v7: "", v8: "" });
+    res.status(200).render("../views/mainpages/index.ejs", { data: "" });
 });
 
+
 router.post("/print", (req, res) => {
-    let F_name = req.body.fname;
-    let l_name = req.body.lname;
-    let add = req.body.address;
-    let City = req.body.city;
-    let state = req.body.state;
-    let post = req.body.postalcode;
-    let amount = req.body.amt;
-    let qty = req.body.qty;
-    res.status(200).render("../views/mainpages/index.ejs", { v1: F_name, v2: l_name, v3: add, v4: City, v5: state, v6: post, v7: amount, v8: qty });
+    let userdata = {
+        F_name: req.body.fname,
+        l_name: req.body.lname,
+        add: req.body.address,
+        City: req.body.city,
+        state: req.body.state,
+        post: req.body.postalcode,
+        amount: req.body.amt,
+        qty: req.body.qty
+    }
+    console.log(userdata);
+    res.status(200).render("../views/bill/bill.ejs", { data: userdata });
+    // res.status(200).render("../views/mainpages/index.ejs", { v1: F_name, v2: l_name, v3: add, v4: City, v5: state, v6: post, v7: amount, v8: qty });
 })
 
 module.exports = router;
